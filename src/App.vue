1b777779764d58fdb3c83f731066f5af2cc49a9f
Marcel Iskrač <marceliskra@gmail.com>
2023-07-21 11:59:17 +0200

    Implemented new features
    - generate PDF
    - generate ZIP

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: green;
+  background-color: #f7f7f7;
   height: 100%;
 }
 </style>
--- src/components/BarcodeGenerator.vue
+++ src/components/BarcodeGenerator.vue
@@ -1,20 +1,36 @@
 <template>
 	<div class="main-container">
-		<div class="input-section">
-			<textarea v-model="inputText" @input="generateBarcodes" placeholder="Enter text..."></textarea>
+		<h1 class="title">Barcode Generator</h1>
+		<div class="dropdown-container">
+			<label for="barcode-format">Select Barcode Format:</label>
+			<select id="barcode-format" v-model="selectedFormat" @change="generateBarcodes">
+				<option value="CODE128">CODE128</option>
+				<option value="CODE39">CODE39</option>
+				<option value="EAN8">EAN8</option>
+				<option value="MSI">MSI</option>
+				<option value="CODABAR">CODABAR</option>
+				<!-- Add more barcode format options here -->
+			</select>
 		</div>
-		<div class="barcodes-section">
-			<div class="barcodes-scrollable">
-				<template v-for="(line, index) in inputLines" :key="index">
-					<div v-if="line.startsWith('//')" class="barcode-text">
-						<h3>{{ line.substring(2) }}</h3>
-					</div>
-					<div v-else class="barcode-item">
-						<img :src="barcodeList[index]" alt="Barcode" />
-					</div>
-					<br />
-				</template>
-				<!-- <canvas v-for="(barcodeData, index) in barcodeList" :key="index" :ref="'barcodeCanvas' + index"></canvas> -->
+		<button @click="generatePDF">Generate PDF</button>
+		<button @click="generateZip">Generate ZIP</button>
+		<div class="generator-container">
+			<div class="input-section">
+				<textarea v-model="inputText" @input="generateBarcodes" placeholder="Enter code value..."></textarea>
+			</div>
+			<div class="barcodes-section">
+				<div class="barcodes-scrollable" ref="pdfContainer">
+					<template v-for="(line, index) in inputLines" :key="index">
+						<div v-if="line.startsWith('//')" class="barcode-text">
+							<h3>{{ line.substring(2) }}</h3>
+						</div>
+						<div v-else class="barcode-item">
+							<img :src="barcodeList[index]" alt="Barcode" />
+						</div>
+						<br />
+					</template>
+					<!-- <canvas v-for="(barcodeData, index) in barcodeList" :key="index" :ref="'barcodeCanvas' + index"></canvas> -->
+				</div>
 			</div>
 		</div>
 	</div>
@@ -22,12 +38,15 @@
   
 <script>
 import JsBarcode from "jsbarcode";
+import html2pdf from "html2pdf.js";
+import JSZip from "jszip";
 
 export default {
 	data() {
 		return {
 			inputText: "",
 			barcodeList: [],
+			selectedFormat: "CODE128" // default
 		};
 	},
 	computed: {
@@ -43,16 +62,75 @@ export default {
 					this.barcodeList.push(null);
 				} else {
 					const canvas = document.createElement("canvas");
-					JsBarcode(canvas, line, {
-						format: "CODE128", // Change this to other formats like "CODE39", "EAN13", etc.
-						displayValue: true,
-						fontSize: 16,
-						margin: 10,
-					});
 
-					this.barcodeList.push(canvas.toDataURL());
+					try {
+						JsBarcode(canvas, line, {
+							format: this.selectedFormat, // Change this to other formats like "CODE39", "EAN13", etc.
+							displayValue: true,
+							fontSize: 16,
+							margin: 10,
+						});
+
+						this.barcodeList.push(canvas.toDataURL());
+					} catch (e) {
+						console.log(e);
+					}
 				}
 			});
+		},
+		generatePDF() {
+			if (this.inputText !== "") {
+				const element = this.$refs.pdfContainer;
+				const options = {
+					margin: 0,
+					filename: "barcodes.pdf",
+					image: {
+						type: "jpeg",
+						quality: 0.98,
+					},
+					html2canvas: {
+						scale: 2,
+					},
+					jsPDF: {
+						unit: "mm",
+						format: "a4",
+						orientation: "portrait",
+					},
+				};
+
+				html2pdf().set(options).from(element).save();
+			}
+		},
+		async generateZip() {
+			if (this.inputText !== "") {
+
+				const zip = new JSZip();
+
+				for (let i = 0; i < this.inputLines.length; i++) {
+					const line = this.inputLines[i];
+
+					if (!line.startsWith("//")) {
+						const canvas = document.createElement("canvas");
+						JsBarcode(canvas, line, {
+							format: this.selectedFormat,
+							displayValue: true,
+							fontSize: 16,
+							margin: 10,
+						});
+
+						const base64Image = canvas.toDataURL().split(";base64,").pop();
+						const filename = `${line.trim()}`;
+						zip.file(`${filename}.png`, base64Image, { base64: true });
+					}
+				}
+
+				const zipBlob = await zip.generateAsync({ type: "blob" });
+				const zipURL = URL.createObjectURL(zipBlob);
+				const a = document.createElement("a");
+				a.href = zipURL;
+				a.download = "barcodes.zip";
+				a.click();
+			}
 		},
 	},
 };
@@ -60,14 +138,40 @@ export default {
   
 <style>
 .main-container {
-	background-color: lightgray;
+	background-color: #f7f7f7;
 	height: 100%;
 	width: 60%;
 	margin: 0 auto;
+}
+
+.generator-container {
+	height: 83%;
 	display: flex;
 	justify-content: space-between;
 	align-items: stretch;
-	flex-wrap: wrap;
+}
+
+.title {
+	text-align: center;
+	margin: 0px;
+	padding: 0px;
+	padding-top: 20px;
+}
+
+.dropdown-container {
+	text-align: center;
+	margin-bottom: 10px;
+}
+
+.dropdown-container label {
+	margin-right: 10px;
+}
+
+.dropdown-container select {
+	padding: 5px;
+	border-radius: 3px;
+	width: 200px;
+	text-align: center;
 }
 
 .input-section {
@@ -82,6 +186,8 @@ textarea {
 	width: 100%;
 	min-height: 0;
 	resize: none;
+	border-radius: 3px;
+	font-size: 16px;
 	padding: 10px;
 	box-sizing: border-box;
 	overflow-y: auto;
@@ -97,13 +203,14 @@ textarea {
 }
 
 .barcodes-scrollable {
-	max-height: calc(100vh - 40px);
-	min-height: calc(100vh - 35px);
-	border: 1px solid black;
+	max-height: calc(100% - 40px);
+	min-height: calc(100% - 35px);
+	border: 1px solid gray;
 	border-radius: 3px;
 	margin-right: 10px;
 	padding: 5px;
 	width: 100%;
+	background-color: white;
 	overflow-y: auto;
 }
 
@@ -129,6 +236,23 @@ br {
 	margin: 0;
 	line-height: 1px;
 	content: "";
+}
+
+button {
+	margin: 4px;
+	padding: 10px 20px;
+	font-size: 16px;
+	background-color: #007bff;
+	color: #fff;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #0056b3;
 }
 
 </style>
